d446d9cb318dd781fa9375820dc2298d6b4b54ed
eandualem <eandualem@gmail.com>
2021-09-10 09:16:47 +0300

    feat: update home page

--- src/view/pages/home.js
+++ src/view/pages/home.js
@@ -1,8 +1,56 @@
 import { Container } from "@material-ui/core";
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import {Card, Title, Fab, Actions} from "../components"
+import Typography from '@material-ui/core/Typography';
+
+let x = "አገራችን ከአፍሪካም ሆነ ከሌሎች የአለም አገራት ጋር ያላትን አለም አቀፋዊ ግንኙነት ወደ ላቀ ደረጃ ያሸጋገረ ሆኗል በአገር ውስጥ አራት አለም ጀልባያውም የወረቀት"
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+	transcription: {
+    fontSize: 24,
+  },
+}));
 
 function Home() {
-	return <Container>...Loading</Container>;
+	const classes = useStyles();
+
+  const record = () => {
+    console.log("Record");
+  }
+	const stop = () => {
+    console.log("stop");
+  }
+	const reload = () => {
+    console.log("reload");
+  }
+	const next = () => {
+    console.log("next");
+  }
+
+	return (
+    <Container component="main" maxWidth="xs">      
+      <div className={classes.paper}>
+        <Title>
+          Welcome
+        </Title>
+        <Card>
+					<Typography className={classes.transcription} color="textSecondary" align="center" gutterBottom>
+          	{x}
+        	</Typography>
+				</Card>
+
+				<Actions recordHandler={record} stopHandler={stop} reloadHandler={reload}  nextHandler={next}></Actions>
+        
+      </div>
+    </Container>
+  );
 }
 
 export default Home;
