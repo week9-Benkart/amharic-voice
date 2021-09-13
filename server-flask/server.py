ca1d53435f8fd54c0da3bb9c690d590a48d3c6a9
eandualem <eandualem@gmail.com>
2021-09-13 22:26:34 +0300

    feat: add kafka-helper to flask-server

--- server-flask/server.py
+++ server-flask/server.py
@@ -4,6 +4,8 @@ import os
 from flask import request
 from flask import Flask, url_for
 from flask_cors import CORS
+from kafka_helper import KafkaHelper
+kh = KafkaHelper()
 
 app = Flask(__name__)
 CORS(app)
@@ -12,14 +14,16 @@ CORS(app)
 def index():
     if request.method == 'GET':
         # randomly select text and send to user
-        text = "የተሻለ ብቃት ያሳዩ ቦክሰኞች ለቶኪዮ ኦሊምፒክ ማጣሪያ ተሳታፊ እንደሚሆኑም ታውቋል"
-        return text
+
+        # text = "የተሻለ ብቃት ያሳዩ ቦክሰኞች ለቶኪዮ ኦሊምፒክ ማጣሪያ ተሳታፊ እንደሚሆኑም ታውቋል"
+        return kh.get_transcription()
     if request.method == "POST":
         f = request.files['wavFile']
         with open('audio.wav', 'wb') as audio:
             # send the file to kafka
             f.save(audio)
         print('file uploaded successfully')
+        kh.send_audio()
         return("Works")
 
 if __name__ == '__main__':
