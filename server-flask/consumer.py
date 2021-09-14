e724b2c4fe989ce68a1cdae322ee8da91f615f88
eandualem <eandualem@gmail.com>
2021-09-14 10:22:51 +0000

    feat: update kafka producer

---
+++ server-flask/consumer.py
@@ -0,0 +1,54 @@
+from kafka import KafkaConsumer
+import json
+import numpy as np
+import time
+
+
+class Consumer:
+    """ Class for Consumer"""
+
+    def __init__(self, bootstrap='b-1.demo-cluster-1.9q7lp7.c1.kafka.eu-west-1.amazonaws.com:9092', topic='example_topic_group2', file_type="text"):
+        """ Initialize consumer with the given bootstrapserver"""
+        self.bootstrap = bootstrap
+        self.topic = topic
+        self.file_type = file_type
+        self.trans = []
+
+    def get_transcription_from_kafka(self):
+        """ Initialize Consumer"""
+        consumer = KafkaConsumer( self.topic,
+                                  bootstrap_servers=[self.bootstrap],
+                                  auto_offset_reset='earliest',
+                                  group_id='benkart-group',
+                                  )
+        messages = consumer.poll(timeout_ms=1000, max_records=10)
+        if(messages=={}):
+          print("Error -")
+          return
+
+        for tp, mess in messages.items():
+          for message in mess:
+            self.trans.append(message.value.decode('utf-8'))
+          self.trans.reverse()
+          consumer.close()
+          return
+
+    def get_transcription(self):
+      if(len(self.trans)==0):
+        self.get_transcription_from_kafka()
+      text = self.trans.pop()
+      return text
+
+    def print_msg(self):
+        """ Print text messages from consumer"""
+        for message in self.consumer:
+            if self.file_type == "text":
+                print ("%s:%d:%d: value=%s" % (message.topic, message.partition,
+                                          message.offset,message.value.decode('utf-8')))
+
+            elif self.file_type == "audio":
+                print ("%s:%d:%d: value=%s" % (message.topic, message.partition,
+                                      message.offset,np.frombuffer(message.value, dtype=np.float32)))
+
+            else:
+                print("error")
--- server-flask/server.py
+++ server-flask/server.py
@@ -4,19 +4,17 @@ import os
 from flask import request
 from flask import Flask, url_for
 from flask_cors import CORS
-from kafka_helper import KafkaHelper
-kh = KafkaHelper()
+from consumer import Consumer
+consumer = Consumer()
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route("/api", methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        # randomly select text and send to user
-
-        # text = "የተሻለ ብቃት ያሳዩ ቦክሰኞች ለቶኪዮ ኦሊምፒክ ማጣሪያ ተሳታፊ እንደሚሆኑም ታውቋል"
-        return kh.get_transcription()
+        return consumer.get_transcription()
     if request.method == "POST":
         f = request.files['wavFile']
         with open('audio.wav', 'wb') as audio:
